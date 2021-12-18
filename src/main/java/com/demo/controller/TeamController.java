e9a83e99de905c1be4e703d2643eafcd49c00b05
卷心菜 <332209078@qq.com>
2021-12-18 22:01:32 +0800

    增加了team接口

--- src/main/java/com/demo/controller/TeamController.java
+++ src/main/java/com/demo/controller/TeamController.java
@@ -5,9 +5,11 @@ import com.demo.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/team")
 public class TeamController {
     @Autowired
     private TeamService ts;
