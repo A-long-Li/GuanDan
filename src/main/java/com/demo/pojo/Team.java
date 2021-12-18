3c9b3d51ba665a291291a1aa553780fe3204eede
卷心菜 <332209078@qq.com>
2021-12-18 16:18:34 +0800

    Initial commit

---
+++ src/main/java/com/demo/pojo/Team.java
@@ -0,0 +1,16 @@
+package com.demo.pojo;
+
+import lombok.Data;
+
+@Data
+public class Team {
+    //团队id
+    private int id;
+    //选手1，2的名字
+    private  String player1;
+    private  String player2;
+    //团队分数
+    private int score;
+    //打到哪一个
+    private String idx;
+}
