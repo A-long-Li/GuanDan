8a24d22fb37e2fb71ae392e063c643d5b88a9ed9
卷心菜 <332209078@qq.com>
2021-12-19 15:47:07 +0800

    返回值更新为状态码

--- src/main/java/com/demo/controller/TeamController.java
+++ src/main/java/com/demo/controller/TeamController.java
@@ -3,6 +3,8 @@ package com.demo.controller;
 import com.demo.pojo.Team;
 import com.demo.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,21 +23,26 @@ public class TeamController {
 
     @PostMapping("/add")
     @ResponseBody
-    public int addTeam(Team t){
-        return ts.Add_Team(t);
+    public ResponseEntity<String> addTeam(Team t){
+        int n =  ts.Add_Team(t);
+        if(n > 0)return new ResponseEntity<>("Ok", HttpStatus.OK);
+        return new ResponseEntity<>("False", HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @PostMapping("/count")
     @ResponseBody
-    public int countScore(Team t){
-
-        return  ts.Count_Score(t);
+    public ResponseEntity<String> countScore(Team t){
+        int n =   ts.Count_Score(t);
+        if(n > 0)return new ResponseEntity<>("Ok", HttpStatus.OK);
+        return new ResponseEntity<>("False", HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     @GetMapping("/delete")
     @ResponseBody
-    public int deleteTeam(int id){
-        return ts.Delete_Team(id);
+    public ResponseEntity<String> deleteTeam(int id){
+        int n = ts.Delete_Team(id);
+        if(n > 0)return new ResponseEntity<>("Ok", HttpStatus.OK);
+        return new ResponseEntity<>("False", HttpStatus.SERVICE_UNAVAILABLE);
     }
 
     /*编辑角色详情*/
